b5526be06cad0e847f6c385570cd3c8c6210c305
Andrea Damgaard Hegner <anhe0001@stud.kea.dk>
2023-08-29 13:41:49 +0200

    javascript

--- main.js
+++ main.js
@@ -1,14 +1,33 @@
 //fetch
-fetch("https://kea-alt-del.dk/t7/api/sample")
+fetch("https://kea-alt-del.dk/t7/api/products")
   .then((res) => res.json())
-  .then(showSample);
+  .then(showProducts);
 
-function showSample(sample) {
+function showProducts(products) {
   //loope
-  sample.forEach(showSample);
+  products.forEach(showProduct);
 }
 
-function showSuperhero(sample) {
-  console.log(sample);
-  const template = document.querySelector;
+const template = document.querySelector("template").content;
+const parent = document.querySelector(".grid");
+
+function showProduct(product) {
+  const copy = template.cloneNode(true);
+  console.log(product);
+  copy.querySelector("h3").textContent = product.productdisplayname;
+  copy.querySelector("p.p1").textContent = product.articletype;
+  //copy.querySelector("p.p1").textContent = product.brandname;
+  copy.querySelector("p.p2").textContent = product.price;
+  if (product.discount) {
+    copy.querySelector("p.p3").textContent = product.price - (product.price / 100) * product.discount;
+    copy.querySelector("p.p4").textContent = product.discount + "%";
+  } else {
+    copy.querySelector(".discount").remove();
+  }
+  if (product.soldout) {
+    copy.querySelector("article").classList.add("soldout");
+  }
+  copy.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+
+  parent.appendChild(copy);
 }
